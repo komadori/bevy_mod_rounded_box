ecc4b3d0b186b6baa525e5bb16ef88e0cb3ca3d4
Aztro-dev <davidperezcastellanos@gmail.com>
2024-02-20 22:05:23 -0600

    Fix lighting b/c 0.13 messed up lighting

--- examples/cube.rs
+++ examples/cube.rs
@@ -68,7 +68,7 @@ fn setup(
         .insert(TheCube);
     commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 1500.0,
+            intensity: 500_000.0,
             shadows_enabled: true,
             ..default()
         },
